eb3f4780c3c15ad953c513a8342a0dd336c83363
adelman <samuel.adelman@gmail.com>
2017-04-09 09:38:23 -0400

    hog features training

---
+++ source/train_classifier.py
@@ -0,0 +1,143 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import glob
+import time
+import pickle
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from skimage.feature import hog
+from sklearn.model_selection import train_test_split
+
+classification_folder = '../classification'
+
+class Classifier():
+    # Define a function to return HOG features and visualization
+    def get_hog_features(self, img, orient, pix_per_cell, cell_per_block,
+                         vis=False, feature_vec=True):
+        # Call with two outputs if vis==True
+        if vis == True:
+            features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                      cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True,
+                                      visualise=vis, feature_vector=feature_vec)
+            return features, hog_image
+        # Otherwise call with one output
+        else:
+            features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                           cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True,
+                           visualise=vis, feature_vector=feature_vec)
+            return features
+
+    # Define a function to extract features from a list of images
+    # Have this function call bin_spatial() and color_hist()
+    def extract_features(self, imgs, cspace='RGB', orient=9,
+                         pix_per_cell=8, cell_per_block=2, hog_channel=0):
+        # Create a list to append feature vectors to
+        features = []
+        # Iterate through the list of images
+        for file in imgs:
+            # Read in each one by one
+            image = mpimg.imread(file)
+
+            # Uncomment the following line if you extracted training
+            # data from .png images (scaled 0 to 1 by mpimg) and the
+            # image you are searching is a .jpg (scaled 0 to 255)
+            # image = image.astype(np.float32)/255
+
+            # apply color conversion if other than 'RGB'
+            if cspace != 'RGB':
+                if cspace == 'HSV':
+                    feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+                elif cspace == 'LUV':
+                    feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
+                elif cspace == 'HLS':
+                    feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+                elif cspace == 'YUV':
+                    feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+                elif cspace == 'YCrCb':
+                    feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
+            else:
+                feature_image = np.copy(image)
+
+            # Call get_hog_features() with vis=False, feature_vec=True
+            if hog_channel == 'ALL':
+                hog_features = []
+                for channel in range(feature_image.shape[2]):
+                    hog_features.append(self.get_hog_features(feature_image[:, :, channel],
+                                                         orient, pix_per_cell, cell_per_block,
+                                                         vis=False, feature_vec=True))
+                hog_features = np.ravel(hog_features)
+            else:
+                hog_features = self.get_hog_features(feature_image[:, :, hog_channel], orient,
+                                                pix_per_cell, cell_per_block, vis=False, feature_vec=True)
+            # Append the new feature vector to the features list
+            features.append(hog_features)
+        # Return list of feature vectors
+        return features
+
+    def train(self):
+        # Divide up into cars and notcars
+        car_images = glob.glob(classification_folder + '/vehicles/**/*')
+        extra_images = glob.glob(classification_folder + '/non-vehicles/**/*')
+        print ('Car Images {}.  Extra Images {}.'.format(len(car_images), len(extra_images)))
+
+        colorspace = 'HSV' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+        orient = 9
+        pix_per_cell = 8
+        cell_per_block = 2
+        hog_channel = 1 # Can be 0, 1, 2, or "ALL"
+
+        t=time.time()
+        car_features = self.extract_features(car_images, cspace=colorspace, orient=orient,
+                                pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
+                                hog_channel=hog_channel)
+        notcar_features = self.extract_features(extra_images, cspace=colorspace, orient=orient,
+                                pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
+                                hog_channel=hog_channel)
+        t2 = time.time()
+        print(round(t2-t, 2), 'Seconds to extract HOG features...')
+        # Create an array stack of feature vectors
+        X = np.vstack((car_features, notcar_features)).astype(np.float64)
+        # Fit a per-column scaler
+        X_scaler = StandardScaler().fit(X)
+        # Apply the scaler to X
+        scaled_X = X_scaler.transform(X)
+
+        # Define the labels vector
+        y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+
+        # Split up data into randomized training and test sets
+        rand_state = np.random.randint(0, 100)
+        X_train, X_test, y_train, y_test = train_test_split(
+            scaled_X, y, test_size=0.2, random_state=rand_state)
+
+        print('Using:',orient,'orientations',pix_per_cell,
+            'pixels per cell and', cell_per_block,'cells per block')
+        print('Feature vector length:', len(X_train[0]))
+        # Use a linear SVC
+        svc = LinearSVC()
+        # Check the training time for the SVC
+        t=time.time()
+        svc.fit(X_train, y_train)
+        t2 = time.time()
+        print(round(t2-t, 2), 'Seconds to train SVC...')
+
+        with open('svc_fit.p', 'wb') as f:
+            pickle.dump(svc, f)
+
+        # Check the score of the SVC
+        print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+        # Check the prediction time for a single sample
+        t=time.time()
+        n_predict = 10
+        print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
+        print('For these',n_predict, 'labels: ', y_test[0:n_predict])
+        t2 = time.time()
+        print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
+
+
+if __name__ == '__main__':
+    classifier = Classifier()
+    classifier.train()
